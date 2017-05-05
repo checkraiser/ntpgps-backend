c1c00b8cc9cdc8cf1329c03aa1f494b311bc9c2e
Truong Dung <checkraiser11@gmail.com>
2017-05-05 16:48:33 +0700

    Update Seed

--- db/seeds/production/check_ins.seeds.rb
+++ db/seeds/production/check_ins.seeds.rb
@@ -11,18 +11,16 @@ end
 def info(user)
   "#{user.name} - #{latest_update_time(user)}"
 end
-after "production:users" do
-  CheckIn.delete_all
-  CheckOut.delete_all
-  Location.delete_all
-  today = Time.current
-  lat = 20.870855
-  lng = 106.7102486
-  User.all.each do |u|
-    10.times do |t|
-      lat2 = range(18, 22)
-      lng2 = range(106, 107)
-      CreateCheckIn.call u, lat2, lng2, today + (t.days)
-    end
+CheckIn.delete_all
+CheckOut.delete_all
+Location.delete_all
+today = Time.current
+lat = 20.870855
+lng = 106.7102486
+User.all.each do |u|
+  10.times do |t|
+    lat2 = range(18, 22)
+    lng2 = range(106, 107)
+    CreateCheckIn.call u, lat2, lng2, today + (t.days)
   end
 end
