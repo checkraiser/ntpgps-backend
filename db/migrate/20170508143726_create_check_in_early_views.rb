ea89199857aa36fd066703d1232206264c95f86d
Truong Dung <checkraiser11@gmail.com>
2017-05-09 18:44:30 +0700

    Fix

--- db/migrate/20170508143726_create_check_in_early_views.rb
+++
@@ -1,5 +0,0 @@
-class CreateCheckInEarlyViews < ActiveRecord::Migration
-  def change
-    create_view :check_in_early_views, materialized: true
-  end
-end
--- db/migrate/20170508143806_create_check_out_late_views.rb
+++
@@ -1,5 +0,0 @@
-class CreateCheckOutLateViews < ActiveRecord::Migration
-  def change
-    create_view :check_out_late_views, materialized: true
-  end
-end
