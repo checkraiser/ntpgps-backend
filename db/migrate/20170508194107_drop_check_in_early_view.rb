02463da6272af5bde78426142022795f3ef03d9e
Truong Dung <checkraiser11@gmail.com>
2017-05-09 18:27:50 +0700

    Fix migration

--- db/migrate/20170508194107_drop_check_in_early_view.rb
+++
@@ -1,5 +0,0 @@
-class DropCheckInEarlyView < ActiveRecord::Migration[5.0]
-  def change
-    execute 'DROP MATERIALIZED VIEW check_in_early_views'
-  end
-end
--- db/migrate/20170508194306_drop_check_out_late_views.rb
+++
@@ -1,5 +0,0 @@
-class DropCheckOutLateViews < ActiveRecord::Migration[5.0]
-  def change
-    execute 'DROP MATERIALIZED VIEW check_out_late_views'
-  end
-end
